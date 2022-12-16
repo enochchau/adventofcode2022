4c87422cfdb349f2396ab428a67dc34e24645e07
ec965 <enoch965@gmail.com>
2022-12-15 17:33:46 -0800

    day 15 part 1

--- day15.py
+++ day15.py
@@ -1,27 +1,28 @@
 import re
 
 
-def m_dist(sx, sy, ex, ey) -> int:
-    return abs(sx - ex) + abs(sy - ey)
-
-
 if __name__ == "__main__":
     input_file = "./inputs/day15.txt"
     y_goal = 2000000
     coverage = set()
+    lines: list[tuple[int, int, int, int]] = []
 
     with open(input_file) as file:
         for line in file.readlines():
+            sx, sy, bx, by = list(map(lambda x: int(x), re.findall("-?\\d+", line)))
+            lines.append((sx, sy, bx, by))
 
-            sx, sy, bx, by = list(map(lambda x: int(x), re.findall("\\d+", line)))
-            distance = m_dist(sx, sy, bx, by)
+    for l in lines:
+        sx, sy, bx, by = l
 
-            y_dist = abs(sy - y_goal)
-            x_dist = distance - y_dist
-            if x_dist >= 0:
-                for x in range(sx - x_dist, sx + x_dist + 1):
-                    if (bx, by) == (x, y_goal):
-                        continue
+        # get manhatten distance
+        distance = abs(sx - bx) + abs(sy - by)
+
+        y_dist = abs(sy - y_goal) # from sy move up y_dist
+        x_dist = distance - y_dist
+        if x_dist >= 0:
+            for x in range(sx - x_dist, sx + x_dist + 1):
+                if (bx, by) != (x, y_goal):
                     coverage.add(x)
 
     print(len(coverage))
